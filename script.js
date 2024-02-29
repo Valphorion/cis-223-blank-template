1be775583c017a6e6bb667f390b5bb8a6675b8b1
Valphorion <Matthowe08@icloud.com>
2024-02-29 14:06:11 -0600

    Add button demonstration

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create a button element
+const button = document.createElement("button");
+
+// Create Button Text
+const buttonText = new Text("Click Me!");
+
+// Put button text in the button
+button.appendChild(buttonText);
+
+// Put button in document's body
+document.body.appendChild(button);
